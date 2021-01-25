edd0db38c9e28fd0dc88b19500913b8de2753f7e
KasiaBotkowska <katarzyna.botkowska@gmail.com>
2021-01-25 20:49:23 +0100

    simple calculator in html

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/stat_calculator', methods=['GET', 'POST'])
+def calculator():
+    if request.method == 'GET':
+        #formularz kalkulatora
+        return render_template('calculator.html')
+
+    else:
+        #POST method
+        #zwrot danych z kalkulatora
+
+        pass
+
 
 
 if __name__ == '__main__':
